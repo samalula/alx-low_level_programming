3d36277faddecb2e7cf3666a28dfb9f7a6f39e38
samalula <samrawitalula4@gmail.com>
2023-02-24 03:22:21 -0800

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,16 +7,15 @@
  */
 
 int print_last_digit(int n)
-
 {
 	int last;
 
 	last = n % 10;
-	if (last < 0)
-	{
-	last = last * -1;
-	}
-	_putchar(last * '0');
-	return (last);
 
+	if (last < 0)
+		last = last * -1;
+
+	_putchar(last + '0');
+
+	return (last);
 }
